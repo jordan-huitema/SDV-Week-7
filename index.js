5907d0dc3d79d39915f66af232f84a05338077e6
Jordan Huitema <Jordan-Huitema@live.nmit.ac.nz>
2020-04-28 13:03:28 +1200

    added comments

--- index.js
+++ index.js
@@ -8,48 +8,50 @@ console.log(addTwoNumbers(2,3));
 
 const add = function(a,b) { return a + b };
 
-//hoisting
+//hoisting is when you elevate a variable or function above its scope 
 function MyFunc() { 
     anotherFunc()                                       //'anotherFunc' is being hoisted
-    function anotherFunc() {                            //unless this func is hoisted within Myfunc it will not be run
+    function anotherFunc() {                            //unless this func is hoisted it will not be run when my func is run
         return console.log('Hello')
     }
 }
 
-//--------
+//changing the value of an existing index
 let arr = [ 1, 2, 3, 4, 5];
 console.log(arr);
 
-arr[3] = 'x';
+arr[3] = 'x';                                           //this selecting index 3 and sets its value as 'x'
 console.log(arr);
 
-//--------
-console.log(arr.length);
-
+//checking length of variable 
 let arr1 = ['Name', 'LastName']
 console.log(arr1.length)
+
 //'in' checks if an arr has the input index
 console.log(0 in arr1)                                  //arr1 has an index 0
 console.log(2 in arr1)                                  //arr1 dosnt have an index 2
 
+//setting a named sub array within an existing array 
 let arr2 = []
 
-arr2.test = 111
+arr2.test = 111                                         //this selects arr2 and then sets a sub array called 'test' with a number value of '111'
+arr2.test2 = 'nark'                                     //this does the same but names the sub array test2 and gives it a string value
 console.log(arr2)
 console.log(arr2.test)
 
 //'.slice' returns the index's between the first parameter and the second, if there is no second parameter it will return every index after the first parameter  
-console.log(arr.slice(1,4))                            //returns index 1,2,3
-console.log(arr)                                       //does not alter arr
+console.log(arr.slice(1,4))                             //returns index 1,2,3
+console.log(arr.slice(1))                               //returns index 1 and all proceding index's
+console.log(arr)                                        //does not alter arr
 
-//'.concat' merges two arrays
+//'.concat' clones and adds its parameters to the end of and array
 let mergeArr = arr.concat(arr1)
 console.log(mergeArr)
 
 let = names = ['Ali', 'Aron', 'Dale', 'Asti']
 console.log(names.pop())
 
-console.log(names.push('jordan'))
+console.log(names.push('Jordan'))
 console.log(names)
 
 names.forEach(name => console.log(name))
